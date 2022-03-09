17b0b188743c80fd2aee32441c3c562016c684ae
fnobi <fnobi0.8@gmail.com>
2022-03-09 22:43:19 +0900

    fs.promiseの取り込み方

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-import { writeFile } from "fs/promises";
+import { promises as fs } from "fs";
 import tsIndexerCore, { TSIndexerOptions } from "./core";
 
 async function tsIndexer(opts: TSIndexerOptions & { dest?: string }) {
   const body = tsIndexerCore(opts);
   const { dest } = opts;
   if (dest) {
-    await writeFile(dest, body, { encoding: "utf8" });
+    await fs.writeFile(dest, body, { encoding: "utf8" });
   }
   return body;
 }
